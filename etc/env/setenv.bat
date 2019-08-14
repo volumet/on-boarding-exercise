f2e990aee79dc99991de10a56aea42b5740a0447
TuanNguyen <gtn@elca.vn>
2019-08-14 16:35:54 +0700

    Add servlet to access H2 console.

--- etc/env/setenv.bat
+++ etc/env/setenv.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\java\jdk1.7.0_75
+set JAVA_HOME=D:\tools\java\jdk1.8.0_171
 set M2_HOME=D:\tools\apache-maven-3.2.5
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
 set MAVEN_OPTS=-Xmx1024M -Xss128k -XX:MaxPermSize=512M 
