f3da186474c3b16ee76cc7a407fdcee77f3bf3f7
tcb <tcb@elca.vn>
2020-09-03 16:17:01 +0700

    revert wrong change

--- on-boarding-exercise/src/main/java/vn/elca/training/service/impl/ProjectServiceImpl.java
+++ on-boarding-exercise/src/main/java/vn/elca/training/service/impl/ProjectServiceImpl.java
@@ -1,13 +1,11 @@
 package vn.elca.training.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import vn.elca.training.repository.ProjectRepository;
 import vn.elca.training.model.entity.Project;
+import vn.elca.training.repository.ProjectRepository;
 import vn.elca.training.service.ProjectService;
+
+import java.util.List;
 
 /**
  * @author vlp
