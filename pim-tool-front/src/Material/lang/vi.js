dcb7d498066ab9ae9b326ba410dee7a1f5d4da1b
vnq <heroziber@gmail.com>
2021-03-03 10:25:21 +0700

    US01 + US02 completed frontend (added vi.js)

---
+++ pim-tool-front/src/Material/lang/vi.js
@@ -0,0 +1,86 @@
+export default {
+    header : {
+        name : 'Dự Án Quản Lý Thông Tin',
+    },
+    navigation : {
+        title : 'Danh sách dự án',
+        new : 'Mới',
+        project : 'Dự án',
+        customer : 'Khách hàng',
+        supplier : 'Nhà cung cấp'
+    },
+    errorScreen : {
+        unexpected : 'Lỗi không xác định xảy ra',
+        please : 'Xin hãy',
+        contact : 'liên hệ với quản trị viên của bạn',
+        or : 'hoặc',
+        back : 'quay về mục tìm kiếm dự án'
+    },
+    newWorkSpace : {
+        projectNum : 'Mã Dự Án',
+        projectName : 'Tên Dự Án',
+        customer : 'Khách Hàng',
+        group : 'Nhóm',
+        member : 'Thành Viên',
+        status : 'Trạng Thái',
+        startDate : 'Ngày Bắt Đầu',
+        endDate : 'Ngày Kết Thúc',
+        projectStatus: {
+            NEW : 'Mới',
+            PLA : 'Lên Kế Hoạch',
+            INP : 'Đang Làm',
+            FIN : 'Đã Xong'
+        },
+        default_group_title : 'Chọn một nhóm',
+        member_holder: 'Thành viên'
+    },
+    footer : {
+        submit : 'Nộp',
+        cancel : 'Hủy'
+    },
+    newProject : {
+        newProject: 'Dự Án Mới',
+        editProject: 'Sửa Thông Tin Dự Án'
+    },
+    formError : {
+        mandatory: 'Những Mục Quan Trọng Bị Thiếu (*)'
+    },
+    backEndFieldError : {
+        projectNumberMustNotBeDuplicate: 'Mã Dự Án Không Được Trùng',
+        deadlineMustNotBeGreaterThanProjectFinishingDate: 'Ngày Bắt Đầu Không Được Lớn Hơn Ngày Kết Thúc',
+        projectNumberMustBeExist: 'Mã Dự Án Phải Hợp Lệ',
+        memberMustBeExist: 'Thành Viên Phải Hợp Lệ'
+    },
+    projectList : {
+        projectStatus: {
+            NEW : 'Mới',
+            PLA : 'Lên Kế Hoạch',
+            INP : 'Đang Làm',
+            FIN : 'Đã Xong',
+            ALL : 'Toàn Bộ'
+        },
+        search : 'Tìm Kiếm',
+        number : 'Mã',
+        name : 'Tên',
+        status : 'Trạng Thái',
+        customer : 'Khách Hàng',
+        start_date : 'Ngày B.Đầu',
+        delete : 'Xóa',
+        reset : 'Đặt Lại',
+        status_all : 'Toàn Bộ'
+    },
+    totalSelectedProject : {
+        deleteLine : 'xóa những mục đã chọn',
+        selected : 'dự án đã được chọn!'
+    },
+    fieldError : {
+        project_num_key : 'Mã dự án ',
+        project_name_key : 'Tên dự án ',
+        customer_key : 'Khách hàng ',
+        group_key : 'Nhóm ',
+        member_key : 'Thành viên ',
+        start_date_key : 'Ngày bắt đầu ',
+        end_date_key : 'Ngày kết thúc ',
+        error_line : 'phải điền đầy đủ'
+    }
+}
